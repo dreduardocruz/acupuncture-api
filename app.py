3762470f9ba6dcec353d7e62229516b4752e6e08
dreduardocruz <dreduardocruz@gmail.com>
2025-03-23 04:43:16 -0300

    Fix Flask app port configuration for Cloud Run

--- app.py
+++ app.py
@@ -61,4 +61,5 @@ else:
     print("Pasta static não encontrada!")
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
